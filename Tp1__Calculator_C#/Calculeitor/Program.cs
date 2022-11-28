700b992c66d7c4de536ecd845cc357cedd9a4a51
unknown <Aprendiz@ITAAPRCSRFSD157.FormacionSena.RED>
2022-11-28 13:55:36 -0500

    first commit

---
+++ Tp1__Calculator_C#/Calculeitor/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Num1 = 0; int Num2= 0;
+Console.WriteLine("CALCULATOR IN C#");
+Console.WriteLine("----------------");
+Console.WriteLine("Digite un numero y presione Enter");
+
+Num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite un segundo numero");
+Num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Seleccione una opcion de la Lisrta");
+Console.WriteLine("\ta -add");
+Console.WriteLine("\ts - Sustract");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Divide");
+switch(Console.ReadLine())
+{
+    case "a":
+        Console.WriteLine($"Your result: {Num1} + {Num2}= " + ( Num1+ Num2));
+        break;
+
+    case "s":
+        Console.WriteLine($"Your result: {Num1} - {Num2}= "  + (Num1 - Num2));
+        break;
+    case "m":
+        Console.WriteLine($"Your result: {Num1} * {Num2}= " + (Num1 * Num2));
+        break;
+    case "d":
+        Console.WriteLine($"Your result: {Num1} / {Num2}= " + (Num1 / Num2));
+        break;
+}
+Console.Write("presione una tecla");
+Console.ReadLine();
+
+
+
+
+
+
+
+
---
+++ Tp1__Calculator_C#/sumaConsola/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int Num1, Num2, Resultado;
+Console.WriteLine("ingresar 1er Numero");
+Num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("ingresa 2do Numero");
+Num2 = int.Parse(Console.ReadLine());
+Resultado = Num1 + Num2;
+Console.WriteLine("la suma es: "+ Resultado);
+Console.ReadLine();
